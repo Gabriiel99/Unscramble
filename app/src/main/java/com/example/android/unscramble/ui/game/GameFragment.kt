1ccd9d2a9ab13bb403b4c05a3444cb9c0637cca8
Gabriel <gabriel99luques@gmail.com>
2022-04-27 17:54:51 -0300

    Update the text field - Show errors in text field

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -75,13 +75,19 @@ class GameFragment : Fragment() {
     * Displays the next scrambled word.
     */
     private fun onSubmitWord() {
-        if (viewModel.nextWord()) {
-            updateNextWordOnScreen()
-        }else{
-            showFinalScoreDialog()
+        val playerWord = binding.textInputEditText.text.toString()
+
+        if (viewModel.isUserWordCorrect(playerWord)) {
+            setErrorTextField(false)
+            if (viewModel.nextWord()) {
+                updateNextWordOnScreen()
+            } else {
+                showFinalScoreDialog()
+            }
+        } else {
+            setErrorTextField(true)
         }
     }
-
     /*
      * Skips the current word without changing the score.
      * Increases the word count.
