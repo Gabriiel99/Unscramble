cc298fb706526ae61dcc492195fd7177f7a9446c
Gabriel <gabriel99luques@gmail.com>
2022-04-27 16:36:52 -0300

    Populate ViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -5,19 +5,41 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
     private var score = 0
-    private var currentWordCount = 0
+    private var _currentWordCount = 0
     private var _currentScrambledWord = "test"
 
     val currentScrambledWord: String
         get() = _currentScrambledWord
 
+
+    private var wordsList : MutableList<String> = mutableListOf()
+    private lateinit var currentWord : String
+
     init{
         Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
     }
 
-    override fun OnCleared(){
+    override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            _currentWordCount
+            wordsList.add(currentWord)
+        }
     }
 
 }
